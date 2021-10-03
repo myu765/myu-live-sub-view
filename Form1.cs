2377971fe29c21bec1d4263f1f5ab6a0066c2636
muyou <myu765@vivaldi.net>
2021-10-04 00:54:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myu_live_sub_view
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // クライアント領域を、320 x 880 に固定する
+            Size size = new Size(320, 880);
+            ClientSize = size;
+        }
+    }
+}
